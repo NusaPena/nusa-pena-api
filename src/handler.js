8c499640a71f1a09552e132358547d2477f5d3c3
PenulisKode <160392737+IlhamAlifioD@users.noreply.github.com>
2024-05-28 20:02:01 +0700

    # Fixing eslint, adding handler, adding stories
    A. Fixing ESLint:
    1. Adding new rule (eslintignore)
    
    B. Adding Handler
    1. Get image (small, medium, large).
    2. Get stories list
    3. Get filtered stories list
    4. Get story detail

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,126 @@
+const path = require("path");
+const fs = require("fs");
+
+const storyList = require("./list/story-list");
+const storyDetails = require("./detail/story-detail");
+
 const test = (request, h) => {
      const response = h.response("<h1>Test Page</h1>").type("text/html").code(200);
 
      return response;
 };
 
-module.exports = { test };
+const getSmallImageHandler = (request, h) => {
+     const { imageId } = request.params;     // * http://localhost:9000/images/small/test.jpg
+
+     const imagePath = path.join(__dirname, "images", "small", `${imageId}.jpg`);
+          if (fs.existsSync(imagePath)) {
+               return h.file(imagePath);
+          }
+
+     const response = h.response({
+          status: "fail",
+          message: "Gagal memukan gambar (Small)",
+     });
+
+     response.code(404);
+     return response;
+};
+
+const getMediumImageHandler = (request, h) => {
+     const { imageId } = request.params;     // * http://localhost:9000/images/medium/test.jpg
+
+     const imagePath = path.join(__dirname, "images", "medium", `${imageId}.jpg`);
+          if (fs.existsSync(imagePath)) {
+               return h.file(imagePath);
+          }
+
+     const response = h.response({
+          status: "fail",
+          message: "Gagal memukan gambar (Medium)",
+     });
+
+     response.code(404);
+     return response;
+};
+
+const getLargeImageHandler = (request, h) => {
+     const { imageId } = request.params;     // * http://localhost:9000/images/large/test.jpg
+
+     const imagePath = path.join(__dirname, "images", "large", `${imageId}.jpg`);
+          if (fs.existsSync(imagePath)) {
+               return h.file(imagePath);
+          }
+
+     const response = h.response({
+          status: "fail",
+          message: "Gagal memukan gambar (Large)",
+     });
+
+     response.code(404);
+     return response;
+};
+
+const getAllStoriesHandler = (request, h) => {
+     const { title, category, location } = request.query;
+     let filteredStory = [...storyList];
+
+     if (title) {                    // * http://localhost:9000/list?title=Cinderalas
+          const filteredTitle = title.toLowerCase();
+               filteredStory = filteredStory.filter((story) => story.title.toLowerCase().includes(filteredTitle));
+     }
+
+     if (category) {                // * http://localhost:9000/list?category=Dongeng
+          const filteredType = category.toLowerCase();
+               filteredStory = filteredStory.filter((story) => story.category.toLowerCase().includes(filteredType));
+     }
+
+     if (location) {                // * http://localhost:9000/list?location=Jawa%20Timur
+          const filteredLocation = location.toLowerCase();
+               filteredStory = filteredStory.filter((story) => story.location.toLowerCase().includes(filteredLocation));
+     }
+
+     return h.response({
+          status: "success",
+          data: {
+               stories: filteredStory.map((story) => ({
+                    id: story.id,
+                    title: story.title,
+                    category: story.category,
+                    imageId: story.imageId,
+                    storyDesc: story.storyDesc,
+               })),
+          },
+     }).code(200);
+};
+
+const getStoryDetailById = (request, h) => {
+     const { storyId } = request.params;
+
+     const story = storyDetails.filter((indexStory) => indexStory.id === storyId)[0];
+          if (story !== undefined) {
+               return {
+                    status: "success",
+                    data: {
+                         story,
+                    },
+               };
+          }
+
+     const response = h.response({
+          status: "fail",
+          message: "Detail cerita tidak ditemukan",
+     });
+
+     response.code(404);
+     return response;
+};
+
+module.exports = {
+     test,
+     getSmallImageHandler,
+     getMediumImageHandler,
+	getLargeImageHandler,
+     getAllStoriesHandler,
+     getStoryDetailById,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,42 @@
-const { test } = require("./handler");
+const {
+	test,
+	getSmallImageHandler,
+	getMediumImageHandler,
+	getLargeImageHandler,
+	getAllStoriesHandler,
+	getStoryDetailById,
+} = require("./handler");
 
 const routes = [
 	{
 		method: "*",
 		path: "/",
 		handler: test,
+	},
+	{
+		method: "GET",
+		path: "/images/small/{imageId}",
+		handler: getSmallImageHandler,
+	},
+	{
+		method: "GET",
+		path: "/images/medium/{imageId}",
+		handler: getMediumImageHandler,
+	},
+	{
+		method: "GET",
+		path: "/images/large/{imageId}",
+		handler: getLargeImageHandler,
+	},
+	{
+		method: "GET",
+		path: "/list",
+		handler: getAllStoriesHandler,
+	},
+	{
+		method: "GET",
+		path: "/details/{storyId}",
+		handler: getStoryDetailById,
 	},
 ];
 
