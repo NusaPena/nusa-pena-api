e85676489d9548463f7bf84844943c3e196da6a0
NiaPutri23 <illiniamarika23@gmail.com>
2024-06-15 13:46:05 +0700

    Add Images

---
+++ resizeImages.js
@@ -0,0 +1,43 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const inputDirectory = 'src/images/medium';
+const outputDirectories = {
+  small: 'src/images/small',
+  large: 'src/images/large',
+};
+
+const sizes = {
+  small: 360,
+  large: 1024,
+};
+
+// Membuat direktori output jika belum ada
+Object.values(outputDirectories).forEach(dir => {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+});
+
+fs.readdir(inputDirectory, (err, files) => {
+  if (err) throw err;
+
+  files.forEach(file => {
+    if (path.extname(file) === '.png') {
+      const inputPath = path.join(inputDirectory, file);
+      
+      Object.keys(sizes).forEach(sizeKey => {
+        const size = sizes[sizeKey];
+        const outputPath = path.join(outputDirectories[sizeKey], file);
+
+        sharp(inputPath)
+          .resize(size, size)  // Resize to a square
+          .toFile(outputPath, (err, info) => {
+            if (err) throw err;
+            console.log(`Resized ${file} to ${sizeKey} (${size}x${size})`);
+          });
+      });
+    }
+  });
+});
